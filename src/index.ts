05dd9c918267623f05401f97d2d1d89de5042a88
Sivaram <sivaram@tartlabs.com>
2021-02-20 12:44:30 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
-
-app.get('/', (req, res) => res.send("Helo"))
-app.use('/user', route)
+app.use('', route)
 
 app.listen(process.env.PORT || 3000, () => console.log("server started"))
