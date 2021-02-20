bfae6d1242dfc18d8cc3e81df250eca49d39bd3d
Sivaram <sivaram@tartlabs.com>
2021-02-20 12:32:12 +0530

    build added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import *as express from 'express';
 import { route } from '../route/user_route';
-
+const cors = require('cors')
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => res.send("Helo"))
 app.use('/user', route)
