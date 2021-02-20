6753f74ede285193f0616a63e3c25e3d1e9c79e0
Sivaram <sivaram@tartlabs.com>
2021-02-20 12:15:35 +0530

    get route added

--- route/user_route.ts
+++ route/user_route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 const route = Router()
-
+route.get('/', (req, res) => res.send("Helo"))
 route.get('/get_user', (request, response) => response.send("USers"))
 
 export { route };
